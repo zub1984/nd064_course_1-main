ecc1bc3f60dd68191ea546ebec80ed10e93f554e
Jubair <zub1984.kn@gmail.com>
2021-09-07 00:10:13 +0300

    Suggestion 2

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,64 +1,104 @@
 import sqlite3
 import logging
+from typing import Any
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
 dbCount = 0
+healthy = True
+
 def addCounter():
-     global dbCount
-     dbCount = dbCount + 1 
-     return dbCount
+    global dbCount
+    dbCount = dbCount + 1
+    return dbCount
+
+def getHealth(healthFlag):
+    global healthy
+    healthy = healthFlag
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
+
 def get_db_connection():
-    connection = sqlite3.connect('database.db')
-    connection.row_factory = sqlite3.Row
-    addCounter()
+    try:
+        connection = sqlite3.connect('database.db')
+        connection.row_factory = sqlite3.Row
+        addCounter()
+        getHealth(True)
+
+    except sqlite3.Error as er:
+        app.logger.error('SQLite error: %s' % (' '.join(er.args)))
+        getHealth(False)
+
     return connection
 
 # Function to get a post using its ID
+
+
 def get_post(post_id):
-    connection = get_db_connection()
-    post = connection.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
-    connection.close()
+    post = []
+    try:
+        connection = get_db_connection()
+        post = connection.execute('SELECT * FROM posts WHERE id = ?',
+                                  (post_id,)).fetchone()
+        connection.close()
+        getHealth(True)
+    except sqlite3.Error as er:
+        app.logger.error('SQLite error: %s' % (' '.join(er.args)))
+        getHealth(False)
+
     return post
+
 
 # Define the Flask application
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
-# Define the main route of the web application 
+# Define the main route of the web application
+
+
 @app.route('/')
 def index():
-    app.logger.debug('Main request recevied')
-    connection = get_db_connection()
-    posts = connection.execute('SELECT * FROM posts').fetchall()
-    connection.close()
+    posts = []
+    try:
+        connection = get_db_connection()
+        posts = connection.execute('SELECT * FROM posts').fetchall()
+        connection.close()
+        getHealth(True)
+    except sqlite3.Error as er:
+        app.logger.error('SQLite error: %s' % (' '.join(er.args)))
+        getHealth(False)
+
     return render_template('index.html', posts=posts)
-    
-# Define how each individual article is rendered 
+
+# Define how each individual article is rendered
 # If the post ID is not found a 404 page is shown
+
+
 @app.route('/<int:post_id>')
 def post(post_id):
     app.logger.debug('Fetch individual article.')
     post = get_post(post_id)
     if post is None:
-      app.logger.error('Article not found, post_id=%s , return 404 page.',post_id)
-      return render_template('404.html'), 404
+        app.logger.error(
+            'Article not found, post_id=%s , return 404 page.', post_id)
+        return render_template('404.html'), 404
     else:
-      app.logger.info('Article retrieved, post_id=%s',post_id)
-      return render_template('post.html', post=post)
+        app.logger.info('Article retrieved, post_id=%s', post_id)
+        return render_template('post.html', post=post)
 
 # Define the About Us page
+
+
 @app.route('/about')
 def about():
     app.logger.debug('About us page is retrieved.')
     return render_template('about.html')
 
-# Define the post creation functionality 
+# Define the post creation functionality
+
+
 @app.route('/create', methods=('GET', 'POST'))
 def create():
     app.logger.debug('Received new post.')
@@ -70,26 +110,37 @@ def create():
             app.logger.info('Title is required!')
             flash('Title is required!')
         else:
-            connection = get_db_connection()
-            connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
-            connection.commit()
-            connection.close()
-            app.logger.info('Post inserted, title=%s',title)
+            try:
+                connection = get_db_connection()
+                connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                                   (title, content))
+                connection.commit()
+                connection.close()
+                app.logger.info('Post inserted, title=%s', title)
+                getHealth(True)
+            except sqlite3.Error as er:
+                app.logger.error('SQLite error: %s' % (' '.join(er.args)))
+                getHealth(False)
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
+
 @app.route('/healthz')
 def healthcheck():
-    app.logger.debug('Received healthz request.')
-    response = app.response_class(
-            response=json.dumps({"result":"OK - healthy"}),
-            status=200,
-            mimetype='application/json'
-    )
-    app.logger.info('healthz request successfull')
+    app.logger.debug('Received healthz request, %s', healthy)
+
+    if healthy :
+        app.logger.info('OK - healthy')
+        response = app.response_class(response=json.dumps(
+            {"result": "OK - healthy"}), status=200, mimetype='application/json')
+    else:
+        app.logger.info('ERROR - unhealthy')
+        response = app.response_class(response=json.dumps(
+            {"result": "ERROR - unhealthy"}), status=500, mimetype='application/json')
+
     return response
+
 
 @app.route('/metrics')
 def metrics():
@@ -98,24 +149,23 @@ def metrics():
     post_count = len(connection.execute('SELECT * FROM posts').fetchall())
     connection.close()
     response = app.response_class(
-            response=json.dumps({"status":"success","code":0,"data":{"db_connection_count":dbCount,"post_count":post_count}}),
-            status=200,
-            mimetype='application/json'
+        response=json.dumps({"status": "success", "code": 0, "data": {
+                            "db_connection_count": dbCount, "post_count": post_count}}),
+        status=200,
+        mimetype='application/json'
     )
     app.logger.info('Metrics request successfull')
     return response
 
+
 # start the application on port 3111
 if __name__ == "__main__":
-    ## stream logs to a file
+    # stream logs to a file
     db_connection_count = 0
-    logging.basicConfig(level=logging.DEBUG,format="%(asctime)s [%(levelname)s] %(message)s",
-    handlers=[
-        logging.FileHandler("app.log"),
-        logging.StreamHandler()
-        ]
-    )
-    
+    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s [%(levelname)s] %(message)s",
+                        handlers=[
+                            logging.FileHandler("app.log"),
+                            logging.StreamHandler()
+                        ]
+                        )
     app.run(host='0.0.0.0', port='3111', debug=True)
-  
-
